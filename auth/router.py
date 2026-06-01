170726ac421ad9154c36465863742d1f1ab3eb07
amalt7451 <amalt7451@gmail.com>
2026-06-01 17:23:15 +0530

    docker compose

--- auth/router.py
+++ auth/router.py
@@ -1,5 +1,5 @@
 
-
+import logging
 from fastapi import APIRouter, Depends
 from auth import services as auth_service
 from auth.schemas import TokenResponse
@@ -8,10 +8,13 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.security import OAuth2PasswordRequestForm
 
 
+logger=logging.getLogger(__name__)
+
 router=APIRouter(prefix="/auth",tags=["auth"])
 @router.post("/login",response_model=TokenResponse)
 async def login(form: OAuth2PasswordRequestForm = Depends(), db:AsyncSession= Depends(get_db)):
     token=await auth_service.login(db,form.username,form.password)
+    logger.info(f"User {form.username} logged in successfully")
     return TokenResponse(access_token=token)
 
 
